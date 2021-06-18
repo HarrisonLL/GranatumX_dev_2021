c76f8656e6431e1b99efeb355548f029929d743f
Ubuntu <ubuntu@ns102669.ip-147-135-37.us>
2021-06-18 01:35:24 +0000

    frontend & catch error

--- HCA_Gbox/main.py
+++ HCA_Gbox/main.py
@@ -8,6 +8,7 @@ import shutil
 import copy
 from collections import defaultdict
 
+
 def download_file(url, output_path):
     url = url.replace('/fetch', '')  # Work around https://github.com/DataBiosphere/azul/issues/2908
     
@@ -35,8 +36,33 @@ def iterate_matrices_tree(tree, keys=()):
         assert False
 
 
-def download_data(ProjectID):
-    print(os.getcwd())
+def export_data(gn):
+    os.chdir('./tmp_datasets')
+    #print(len(assay['matrix']),flush = True)
+    #print(len(assay['sampleIds']), flush = True)
+
+    filename = 't-cell-activation-human-blood-10XV2.loom'
+    ds = loompy.connect(filename)
+    tmp = ds[:,:].tolist()
+
+    HCAassay = defaultdict(list)
+
+    print('Storing Data...', flush = True)
+    HCAassay['matrix'] = tmp
+    HCAassay['geneIds'] = ds.ra["Gene"].tolist()
+    HCAassay['sampleIds'] = ds.ca["CellID"].tolist()
+    print('Success!',flush = True)
+
+    print('Exporting data... May take long:>', flush = True)
+
+    #assay_export_name = "[A]{}".format(basename(filename))
+    gn.export(HCAassay,"HCA assay")
+    gn.add_result("Successfully loading HCA data", data_type='markdown')
+
+    gn.commit()
+
+
+def download_data(ProjectID, gn):
     # destroy if exits, and then create ./tmp_datasets directory
     dirpath = './tmp_datasets'
     if os.path.exists(dirpath) and os.path.isdir(dirpath):
@@ -47,22 +73,29 @@ def download_data(ProjectID):
     catalog = 'dcp6'
     endpoint_url = f'https://service.azul.data.humancellatlas.org/index/projects/{project_uuid}'
     save_location = dirpath
-    response = requests.get(endpoint_url, params={'catalog': catalog})
-    response.raise_for_status()
 
-    response_json = response.json()
-    project = response_json['projects'][0]
 
-    file_urls = set()
-    for key in ('matrices', 'contributorMatrices'):
-        tree = project[key]
-        for path, file_info in iterate_matrices_tree(tree):
-            url = file_info['url']
-            if url not in file_urls:
-                dest_path = os.path.join(save_location, file_info['name'])
-                download_file(url, dest_path)
-                file_urls.add(url)
+    try:
+        response = requests.get(endpoint_url, params={'catalog': catalog})
+        response.raise_for_status()
 
+        response_json = response.json()
+        project = response_json['projects'][0]
+
+        file_urls = set()
+        for key in ('matrices', 'contributorMatrices'):
+            tree = project[key]
+            for path, file_info in iterate_matrices_tree(tree):
+                url = file_info['url']
+                if url not in file_urls:
+                    dest_path = os.path.join(save_location, file_info['name'])
+                    download_file(url, dest_path)
+                    file_urls.add(url)
+        print("Finished downloading!", flush = True)
+        export_data(gn)
+    except requests.exceptions.HTTPError:
+        print("Invalid ID entered. Please try again.")
+        gn.commit()
 
 
 def main():
@@ -86,33 +119,9 @@ def main():
     #gn.commit()
     #print(assay_df.head(),flush = True)
 
-    ProjectID = gn.get_arg('PID', '4a95101c-9ffc-4f30-a809-f04518a23803')
-    download_data(ProjectID)
-    print("Finished downloading!", flush = True)
-    print("Now start to load data...", flush = True)
-    os.chdir('./tmp_datasets')
-    #print(len(assay['matrix']),flush = True)
-    #print(len(assay['sampleIds']), flush = True)
+    ProjectID = gn.get_arg('PID')
+    download_data(ProjectID, gn)
 
-    filename = 't-cell-activation-human-blood-10XV2.loom'
-    ds = loompy.connect(filename)
-    tmp = ds[:,:].tolist()
-
-    HCAassay = defaultdict(list)
-
-    print('Storing Data...', flush = True)
-    HCAassay['matrix'] = tmp
-    HCAassay['geneIds'] = ds.ra["Gene"].tolist()
-    HCAassay['sampleIds'] = ds.ca["CellID"].tolist()
-    print('Success!',flush = True)
-
-    print('Exporting data... May take long:>', flush = True)
-    
-    #assay_export_name = "[A]{}".format(basename(filename))
-    gn.export(HCAassay,"HCA assay")
-    gn.add_result("Successfully loading HCA data", data_type='markdown')
-
-    gn.commit()
 
 if __name__ == "__main__":
     main()
