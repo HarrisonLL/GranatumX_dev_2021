fe086519f07ff33f5651655a530ba4913e00194d
Ubuntu <ubuntu@ns102669.ip-147-135-37.us>
2021-06-24 14:47:59 +0000

    update

--- PanglaoDB_Gbox/main.R
+++ PanglaoDB_Gbox/main.R
@@ -10,20 +10,23 @@ source('./granatum_sdk.R')
 # imports can be accessed using keywords specified 
 # in the "injectInto" field of the arguments section
 # of the package.yaml file - in our case, "exampleImport"
-# assay_file <- gn_get_uploaded_file_path("assayFile")
+assay_file <- gn_get_uploaded_file_path("assayFile")
+
 sra_num <- gn_get_arg("SRA_num")
 srs_num <- gn_get_arg("SRS_num")
 url_head <- "https://panglaodb.se/data_dl.php?"
 url_tail <- "filetype=R&datatype=readcounts"
-url_head <- paste(url_head, a, sep = "sra=")
-url_head <- paste(url_head, b, sep = "&srs=")
+url_head <- paste(url_head, sra_num, sep = "sra=")
+url_head <- paste(url_head, srs_num, sep = "&srs=")
 url_link = paste(url_head, url_tail, sep = "&")
 
-file_path = "./tmp_dataset"
-file_name = paste
+file_path = "./"
+file_name = paste(sra_num, srs_num, sep = "-")
+file_name = paste(file_name, "Rdata", sep = ".")
+dl_target = paste(file_path, file_name, sep = "")
+#download.file(url_link, dl_target, mode = "wget")
 
-download.file(url_link, "E:\\PanglaoDB\\sampledata.Rdata")
-
+#tmp <- load(file_name)
 tmp <- load(assay_file)
 genemat <- get(tmp)
 
@@ -37,18 +40,27 @@ genemat <- get(tmp)
 # assay$matrix <- assay$matrix.T
 #print(genemat@Dim[1])
 #print(genemat@Dim[2])
-datamat <- rep(0,genemat@Dim[1]*genemat@Dim[2])
+
+cell_nums = 100
+
+datamat <- rep(0,genemat@Dim[1]*cell_nums)
 #dgcmat <- as.matrix(summary(genemat))
 
 #print(ncol(dgcmat))
 #print(nrow(dgcmat))
 
 for(j in 1:length(genemat@i)){
+	if (genemat@i[j] + 1 >genemat@Dim[1] * cell_nums){
+       		break
+	}	
 	datamat[genemat@i[j] + 1] = genemat@x[j]
 }
-dim(datamat) <- c(genemat@Dim[1],genemat@Dim[2])
+
+print("dimension transform")
+
+dim(datamat) <- c(genemat@Dim[1],cell_nums)
 Pangassay <- list(matrix = datamat,
-		  sampleIds = dimnames(genemat)[2],
+		  sampleIds = dimnames(genemat)[2][1:cell_nums],
 		  geneIds = dimnames(genemat)[1])
 # export results
 # here, we use keywords specified in the "extractFrom"
