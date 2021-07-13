2f4e598accc55ab839eb3f36cfe9c8352c39e1fa
LidingLi <32115568+LidingLi@users.noreply.github.com>
2021-07-13 11:46:11 -0400

    Add files via upload

--- Memory_Usage_Prediction/README.md
+++ Memory_Usage_Prediction/README.md
@@ -1,9 +1,5 @@
-Run the data_download.py to get the loom file.
+Run the script in commmand line:
+$ python mup.py
 
-Then install deepimpute:
+To install deepimpute:
 $ pip install deepimpute
-
-Then Run the mup.py:
-$ python mup.py --input_file_path ./hnscc-immune-landscape-human-blood-10XV2.loom --gene_size 3000 \
-The script will output sugggested cell size and a regression png file.
-
--- Memory_Usage_Prediction/mup.py
+++ Memory_Usage_Prediction/mup.py
@@ -1,5 +1,7 @@
 from deepimpute.multinet import MultiNet
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.metrics import mean_squared_error
 import loompy
 import pandas as pd
 import numpy as np
@@ -7,6 +9,7 @@ import argparse
 import psutil
 from resource import getrusage, RUSAGE_SELF
 import matplotlib.pyplot as plt
+np.random.seed(12345)
 
 
 def get_ava_memory():
@@ -21,6 +24,21 @@ def get_peak_memory():
     print("Peak memory (MB):", PEAK, flush=True)
     return PEAK
 
+
+def generate_data_matrix(cell_sizes, gene_sizes, percent_nz):
+    datasets = {}
+    for percent in percent_nz:
+        percent = percent / 10
+        for csize in cell_sizes:
+            for gsize in gene_sizes:
+                matrix = np.zeros((csize*gsize,))
+                indices = np.random.randint(0,csize*gsize,size=int(csize*gsize*percent))
+                for i in indices:
+                    matrix[i] = np.random.randint(0,10) # FIX ME
+                matrix = matrix.reshape((gsize, csize))
+                datasets[(gsize, csize, percent)] = matrix
+    return datasets
+    
 
 def run_deep_impute(data):
     # Using custom parameters
@@ -42,43 +60,54 @@ def run_deep_impute(data):
     PEAK = get_peak_memory()
     return PEAK
 
+"""fit linear regression, quadratic linear regression, cubic linear regression
+return mse for each model"""
+def model_fitting(X,y):
+    X_new1 = [[x[0], x[1], x[2]] for x in X]
+    reg = LinearRegression().fit(X_new1, y)
+    y_pred1 = reg.predict(X_new1)
+    mse1 = mean_squared_error(y, y_pred1)
 
-def main(argv):
-    file_path = argv[0]
-    gene_size = argv[1]
-    data = loompy.connect(file_path)
+    poly = PolynomialFeatures(degree=2)
+    X_new2 = poly.fit_transform(X_new1)
+    y_pred2 = reg.predict(X_new2)
+    mse2 = mean_squared_error(y, y_pred2)
+
+    poly = PolynomialFeatures(degree=3)
+    X_new3 = poly.fit_transform(X_new1)
+    y_pred3 = reg.predict(X_new3)
+    mse3 = mean_squared_error(y, y_pred3)
+    return mse1, mse2, mse3
+
+
+def main():
+    
     ava_mb = get_ava_memory()
     memory_data = []
     cell_sizes = [500, 1000, 5000, 10000, 15000, 20000]
-    for size in cell_sizes:
-        df = pd.DataFrame((data[:gene_size, :size].T))
-        PEAK = run_deep_impute(df)
+    gene_sizes = [500, 1000, 5000, 10000, 15000, 20000]
+    percent_nz = [0.01, 0.1, 0.5, 1, 10, 20, 50]
+    datasets = generate_data_matrix(cell_sizes, gene_sizes, percent_nz)
+    for data in datasets.values():
+        PEAK = run_deep_impute(pd.DataFrame(data.T))
         memory_data.append(PEAK)
+    mse1, mse2, mse3 = model_fitting(datasets.keys(), memory_data)
+    print(mse1, mse2, mse3)
+
     # run linear regression to get coeff
-    reg = LinearRegression().fit(np.array(cell_sizes).reshape(6,1), np.array(memory_data).reshape(6,))
-    coef = reg.coef_[0]
-    inter = reg.intercept_
-    print("Suggested cell size:", (ava_mb-inter)//coef)
-    plt.figure(figsize=(4, 3))
-    ax = plt.axes()
-    ax.scatter(cell_sizes, memory_data)
-    ax.plot(np.linspace(0, 22000, 220000), reg.predict(np.linspace(0, 22000, 220000).reshape(-1, 1)))
-    plt.savefig("linear_regression.png")
+    # reg = LinearRegression().fit(np.array(cell_sizes).reshape(6,1), np.array(memory_data).reshape(6,))
+    # coef = reg.coef_[0]
+    # inter = reg.intercept_
+    # print("Suggested cell size:", (ava_mb-inter)//coef)
+    # plt.figure(figsize=(4, 3))
+    # ax = plt.axes()
+    # ax.scatter(cell_sizes, memory_data)
+    # ax.plot(np.linspace(0, 22000, 220000), reg.predict(np.linspace(0, 22000, 220000).reshape(-1, 1)))
+    # plt.savefig("linear_regression.png")
 
 
 
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--input_file_path')
-    parser.add_argument('--gene_size')
-    args = parser.parse_args()
-    argv = [None]*2
-    for arg in vars(args):
-        value = getattr(args, arg)
-        if arg == "input_file_path":
-            argv[0] = value
-        elif arg == "gene_size":
-            argv[1] = int(value)
-    main(argv)
+    main()
