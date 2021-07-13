fd49e0b42662321280b6898b6497299dbb30743c
Ubuntu <ubuntu@ns102669.ip-147-135-37.us>
2021-07-13 23:31:05 +0000

    add tqdm

--- Memory_Usage_Prediction/generate_data.py
+++ Memory_Usage_Prediction/generate_data.py
@@ -2,13 +2,14 @@ import numpy as np
 import pandas as pd
 import os
 import shutil
+from tqdm import tqdm
 
 def generate_data_matrix(cell_sizes, gene_sizes, percent_nz):
     datasets = {}
-    for percent in percent_nz:
-        percent = percent / 100
-        for csize in cell_sizes:
-            for gsize in gene_sizes:
+    for csize in tqdm(cell_sizes):
+        for gsize in gene_sizes:
+            for percent in percent_nz:
+                percent = percent / 100
                 matrix = np.zeros((csize*gsize,))
                 indices = np.random.choice(csize*gsize,size=int(csize*gsize*percent),replace=False)
                 for i in indices:
@@ -26,9 +27,11 @@ def main():
 
     cell_sizes = [1000,2000,5000,10000,20000]
     gene_sizes = [100,500,1000,3000,10000]
-    percent_nz = [1,5,10,20] 
+    percent_nz = [1,5,10,20]
+    print("Generating datasets..", flush=True)
     datasets = generate_data_matrix(cell_sizes,gene_sizes,percent_nz)
-    for k,v in datasets.items():
+    print("Saving..",flush=True)
+    for k,v in tqdm(datasets.items()):
         csv_name = "_".join(k)+".csv"
         pd.DataFrame(data=v).to_csv(os.path.join(dirpath,csv_name))
 
