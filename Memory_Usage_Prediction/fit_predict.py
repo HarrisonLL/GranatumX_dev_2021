a7f1a0051a4d3344b7ca3a038e1c886be3e8b78a
LidingLi <32115568+LidingLi@users.noreply.github.com>
2021-07-14 21:59:45 -0400

    Add files via upload

--- Memory_Usage_Prediction/fit_predict.py
+++ Memory_Usage_Prediction/fit_predict.py
@@ -1,43 +1,41 @@
 import pandas as pd
 import numpy as np
+import sklearn
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import cross_val_score
-
+from sklearn.metrics import mean_squared_error
+from sympy import symbols, Eq, solve
+import psutil
 
 """fit linear regression, quadratic linear regression, cubic linear regression
-return cross validaton r square score for each model"""
+return cross validaton r square, mse for each model, coeff for the best model"""
 def model_fitting(X,y):
-    reg = LinearRegression()
-    rs1 = np.mean(cross_val_score(reg, X, y, cv=5))
-
+    y = y/1024/1024
     poly = PolynomialFeatures(degree=2)
     X_new2 = poly.fit_transform(X)
-    reg = LinearRegression()
+    reg = LinearRegression(fit_intercept=False)
     rs2 = np.mean(cross_val_score(reg, X_new2, y, cv=5))
+    mse2 = np.mean(cross_val_score(reg, X_new2, y, cv=5, scoring='neg_mean_squared_error'))
+    return rs2, mse2, reg.fit(X_new2,y).coef_
 
-    poly = PolynomialFeatures(degree=3)
-    X_new3 = poly.fit_transform(X)
-    reg = LinearRegression()
-    rs3 = np.mean(cross_val_score(reg, X_new3, y, cv=5))
-   
-    X_new9 = [[1,x[0],x[0]**2, x[1],x[1]**2, x[2], x[2]**2] for x in X]
-    X_new10 = [[1, 2*x[0]*x[1], 2*x[1]*x[2]] for x in X]
+def pred_cell_size(coef,genesize, percent,ava_mem):
+    x = symbols('x')
+    eq1 = Eq(coef[0] + coef[1]*genesize + coef[2]*x + coef[3]*percent + coef[4]*(genesize**2) + coef[5]*(genesize*x) + coef[6]*(genesize*percent)+ coef[7]*(x**2) + coef[8]*(x*percent)+coef[9]*(percent**2)-ava_mem)
+    return [int(sol) for sol in solve(eq1) if sol>0]
 
-    rs = [rs1, rs2, rs3]
-    for X_new in [ X_new9, X_new10]:
-        reg = LinearRegression()
-        temp = np.mean(cross_val_score(reg, X_new, y, cv=5))
-        rs.append(temp)
-
-    return rs
 
 def main():
     df = pd.read_csv("performance.csv")
-    rs = model_fitting(df.drop(columns=["Peak Memory Usage"]).to_numpy(), df["Peak Memory Usage"].to_numpy())
-    print(rs)
-    print(np.argmax(rs))
+    df = df.drop(columns=["Unnamed: 0" ])
+    rs2, mse2,coef = model_fitting(df.drop(columns=["Peak Memory Usage"]).to_numpy(), df["Peak Memory Usage"].to_numpy())
+    #print(rs2, mse2,coef)
+    ava_mem = (psutil.virtual_memory()).available/(1024*1024)
+    print("Current available memory is %.3f GB"%(ava_mem/1024))
+    print("If the gene size is 8000, and percent of non-zeros is 10%, we can chunk cell size to")
+    print()
+    print(pred_cell_size(coef, 8000, 10, ava_mem))
 
 
 if __name__ == "__main__":
