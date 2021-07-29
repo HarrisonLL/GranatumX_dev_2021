e22784a4d843bbc59190fd0a641e2dc3232358de
Ubuntu <ubuntu@ns102669.ip-147-135-37.us>
2021-07-29 13:46:52 +0000

    update

--- test_gboxes/test_deepimpute/run_deepimpute.py
+++ test_gboxes/test_deepimpute/run_deepimpute.py
@@ -3,12 +3,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from deepimpute.deepImpute import deepImpute 
+import time
+import json
+import gzip
+from io import StringIO, BytesIO
+import base64
+import gc
 
+def decode_json(jsonstr):
+    bio = BytesIO()
+    stream = BytesIO(base64.b64decode(jsonstr.encode('utf-8')))
+    decompressor = gzip.GzipFile(fileobj=stream, mode='r')
+
+    while True:
+        chunk = decompressor.read(8192)
+        if not chunk:
+            decompressor.close()
+            bio.seek(0)
+            output = bio.read().decode("utf-8")
+            break
+        bio.write(chunk)
+    return json.loads(output)
 
 def main():
     gn = granatum_sdk.Granatum()
+    start_time = time.time()
+    #firstly save to chunks
+    chunks = gn.get_import("assay")
+    print(len(chunks.keys()), flush = True)
 
-    assay = gn.get_import("assay")
+    #assay = gn.get_import("assay")
+    assay = decode_json(chunks["chunk1"])
     data = np.array(assay.get("matrix")).T
 
     seed = gn.get_arg("seed")
@@ -21,57 +46,73 @@ def main():
     #model.fit(data, NN_lim=NN_lim, cell_subset=cell_subset)
 
     frameddata = pd.DataFrame(data)
+    print("imputed", flush = True)
     imputed, model = deepImpute(frameddata, NN_lim=NN_lim, cell_subset=cell_subset)
 
     LABELS_PARAMS = {"fontsize": 14, "fontweight": "bold", "fontname": "serif"}
+    print("Calculate vmax", flush = True)
+    
+    #tmp data, imputed
+    data = data + 1
+    imputed = imputed + 1
 
-    vmax = np.percentile(np.log10(1 + data.flatten()), 99)
+    vmax = np.percentile(np.log10(data.flatten()), 99)
 
-    print("Generating Heatmap")
+    print("Generating Heatmap", flush = True)
     fig, ax = plt.subplots(1, 2)
-    ax[0].imshow(np.log10(1 + data), aspect="auto", vmax=vmax)
-    ax[1].imshow(np.log10(1 + imputed), aspect="auto", vmax=vmax)
+    ax[0].imshow(np.log10(data), aspect="auto", vmax=vmax)
+    ax[1].imshow(np.log10(imputed), aspect="auto", vmax=vmax)
     ax[0].set_xlabel("Genes", **LABELS_PARAMS)
     ax[1].set_xlabel("Genes", **LABELS_PARAMS)
     ax[0].set_ylabel("Cells", **LABELS_PARAMS)
     ax[0].set_title("raw (log)", **LABELS_PARAMS)
     ax[1].set_title("imputed (log)", **LABELS_PARAMS)
+    
+    print("Heatmap created!", flush = True)
 
     gn.add_current_figure_to_results("Heatmaps")
     nb_genes = len(set(model.targets.flatten()))
     #nb_genes = np.sum([len(net.targetGenes) for net in model.targets])
-
+    print("finished adding", flush = True)
+    del data
+    gc.collect()
     def calc_dropout(matrix):
         return np.sum((np.array(matrix) == 0)) * 1. / data.size
 
-    r, p = model.score(frameddata)
-    rows, cols = frameddata.shape
+   # r, p = model.score(frameddata)
+   # rows, cols = frameddata.shape
+    print("start to transform", flush = True)
+    #transform data, imputed back
+    #data = data - 1
+    imputed = imputed - 1
+    #print("creating message", flush = True)
 
-    message = "\n".join(
-        [
-            "  - Data frame number of rows: **{0}**",
-            "  - Data frame number of columns: **{1}**",
-            "  - Number of imputed genes: **{2}**",
-            "  - Percentage of dropout entries *before* imputation: **{3:.2f}%**",
-            "  - Percentage of dropout entries *after* imputation: **{4:.2f}%**",
-            "  - Accuracy (correlation) on masked data: **{5:.2f}**"
-        ]
-    ).format(
-        rows,
-        cols,
-        nb_genes,
-        calc_dropout(data) * 100,
-        calc_dropout(imputed.to_numpy()) * 100,
-	r
-    )
+    #message = "\n".join(
+    #    [
+    #        "  - Data frame number of rows: **{0}**",
+    #        "  - Data frame number of columns: **{1}**",
+    #        "  - Number of imputed genes: **{2}**",
+    #        "  - Percentage of dropout entries *before* imputation: **{3:.2f}%**",
+    #        "  - Percentage of dropout entries *after* imputation: **{4:.2f}%**",
+    #        "  - Accuracy (correlation) on masked data: **{5:.2f}**"
+    #    ]
+    #).format(
+    #    rows,
+    #    cols,
+    #    nb_genes,
+    #    calc_dropout(data) * 100,
+    #    calc_dropout(imputed.to_numpy()) * 100,
+    #   r
+    #)
 
-    gn.add_result(message, data_type="markdown")
+    #gn.add_result(message, data_type="markdown")
 
     assay["matrix"] = imputed.T.to_numpy().tolist()
     gn.export_statically(assay, "Imputed assay")
 
     gn.commit()
-
+    print("--- %s seconds ---" % (time.time() - start_time), flush = True)
+    time.sleep(10)
 
 if __name__ == "__main__":
     main()
