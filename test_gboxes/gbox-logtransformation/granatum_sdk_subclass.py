275bae35afa8273d075dc1e44798218a0ff99224
Minghaoy97 <71526139+Minghaoy97@users.noreply.github.com>
2021-07-29 23:02:56 -0400

    update

--- test_gboxes/gbox-logtransformation/granatum_sdk_subclass.py
+++ test_gboxes/gbox-logtransformation/granatum_sdk_subclass.py
@@ -1,4 +1,4 @@
-from  granatum_sdk import Granatum
+from granatum_sdk import Granatum
 import base64
 from io import StringIO, BytesIO
 import gzip
@@ -26,16 +26,65 @@ class granatum_extended(Granatum):
         self.gbox_name = gbox_name
 
 
-    def check_chunk(self, json_dict):
-        return "current chunk" in json_dict
+    def check_chunk(self, assay):
+        return "current chunk" in assay
 
 
-    def decode_json(jsonstr):
+    def adjust_transform(self, assay):
+        # user rerun the gbox
+        if assay["current chunk"][0] == self.gbox_name:
+            return
+        # four situations
+        if assay["current chunk"][-1] == assay["suggested chunk"].get(self.gbox_name)[0] == "col":
+            pass
+        elif assay["current chunk"][-1] == assay["suggested chunk"].get(self.gbox_name)[0] == "row":
+            pass
+        elif assay["current chunk"][-1] == "col" and assay["suggested chunk"].get(self.gbox_name)[0] == "row":
+            new_file = dict()
+            for i in range(len(assay)-3):
+               chunk = self.decompress_chunk(assay["chunk"+str(i)])
+               org_num_row = assay["origin data size"][0]
+               sug_num_row = assay["suggested chunk"].get(self.gbox_name)[1]
+               count = 0
+               for j in range(0, org_num_row, sug_num_row):
+                   new_assay = {
+                                "matrix":chunk["matrix"][j:j+sug_num_row],
+                                "geneIds":chunk["geneIds"][j:j+sug_num_row],
+                                "sampleIds":chunk["sampleIds"]
+                           }
+                   count += 1
+                   if "chunk"+str(count) in new_file:
+                       new_file["chunk"+str(count)] += [self.compress_chunk(new_assay)]
+                   else:
+                       new_file["chunk"+str(count)] = [self.compress_chunk(new_assay]]
+            return new_file
+
+        elif json_dict["current chunk"][-1] == "row" and json_dict["suggested chunk"].get(self.gbox_name)[0] == "col":
+            new_file = dict()
+            for i in range(len(assay)-3):
+               chunk = self.decompress_chunk(assay["chunk"+str(i)])
+               org_num_col = assay["origin data size"][0]
+               sug_num_col = assay["suggested chunk"].get(self.gbox_name)[1]
+               count = 0
+               for j in range(0, org_num_col, sug_num_col):
+                   new_assay = {
+                                "matrix":np.array(chunk["matrix"][:, j:j+sug_num_col]).tolist(),
+                                "geneIds":chunk["geneIds"],
+                                "sampleIds":chunk["sampleIds"][j:j+sug_num_col]
+                           }
+                   count += 1
+                   if "chunk"+str(count) in new_file:
+                       new_file["chunk"+str(count)] += [self.compress_chunk(new_assay)]
+                   else:
+                       new_file["chunk"+str(count)] = [self.compress_chunk(new_assay]]
+            return new_file
+
+
+    def decompress_chunk(self, pram1):
         bio = BytesIO()
-        stream = BytesIO(base64.b64decode(jsonstr.encode('utf-8')))
+        stream = BytesIO(base64.b64decode(pram1.encode('utf-8')))
         decompressor = gzip.GzipFile(fileobj=stream, mode='r')
-
-        while True:
+        while True:  # until EOF
             chunk = decompressor.read(8192)
             if not chunk:
                 decompressor.close()
@@ -46,36 +95,18 @@ class granatum_extended(Granatum):
         return json.loads(output)
 
 
-    def adjust_transform(self, json_dict):
-        # user rerun the gbox
-        if json_dict["current chunk"][0] == self.gbox_name:
-            return
-        # four situations
-        if json_dict["current chunk"][-1] == json_dict["suggested chunk"].get(self.gbox_name)[0] == "col":
-            pass
-        elif json_dict["current chunk"][-1] == json_dict["suggested chunk"].get(self.gbox_name)[0] == "row":
-            pass
-        elif json_dict["current chunk"][-1] == "col" and json_dict["suggested chunk"].get(self.gbox_name)[0] == "row":
-            #for i in range(len(json_dict)-3):
-                # decompress
-            pass
-        elif json_dict["current chunk"][-1] == "row" and json_dict["suggested chunk"].get(self.gbox_name)[0] == "col":
-            pass
-
-
-    def iterate_chunks(self, json_dict):
-        for k,v in json_dict.items():
-            tmp = base64.b64decode(v)
-            bio = BytesIO()
-            stream = BytesIO(base64.b64decode(b.encode('utf-8')))
-            decompressor = gzip.GzipFile(fileobj=stream, mode='r')
-            while True:  # until EOF
-                chunk = decompressor.read(8192)
-                if not chunk:
-                    decompressor.close()
-                    bio.seek(0)
-                    output = bio.read().decode("utf-8")
-                    break
-                bio.write(chunk)
-            # yield an assay type
-            yield output
+    def compress_chunk(self, pram1):
+        tmp = json.dumps(pram1)
+        bio = BytesIO()
+        bio.write(tmp.encode('utf-8'))
+        bio.seek(0)
+        stream = BytesIO()
+        compressor = gzip.GzipFile(fileobj=stream, mode = 'w')
+        while True:
+            chunk = bio.read(8192)
+            if not chunk:
+                compressor.close()
+                break
+            compressor.write(chunk)
+        encoded = base64.b64encode(stream.getvalue())
+        return(encoded.decode('utf-8'))
