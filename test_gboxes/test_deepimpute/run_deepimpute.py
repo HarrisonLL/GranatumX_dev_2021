2688264973995e5686c64ab903688b335169f6ed
Ubuntu <ubuntu@ns102669.ip-147-135-37.us>
2021-08-06 02:05:04 +0000

    debugging run_deepimpute

--- test_gboxes/test_deepimpute/run_deepimpute.py
+++ test_gboxes/test_deepimpute/run_deepimpute.py
@@ -1,8 +1,9 @@
-import granatum_sdk
+from granatum_sdk_subclass import granatum_extended
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from deepimpute.deepImpute import deepImpute 
+from deepimpute.multinet import MultiNet
 import time
 import json
 import gzip
@@ -26,6 +27,7 @@ def decode_json(jsonstr):
         bio.write(chunk)
     return json.loads(output)
 
+
 def compress_assay(exported_assay):
     tmp = json.dumps(exported_assay)
     bio = BytesIO()
@@ -42,110 +44,105 @@ def compress_assay(exported_assay):
     encoded = base64.b64encode(stream.getvalue())
     return(encoded.decode('utf-8'))
 
+
 def main():
     start_time = time.time()
-    gn = granatum_sdk.Granatum()
-    output = {}
+    gn = granatum_extended("deepimpute")
+    #output = {}
     
     #firstly save to chunks
     chunks = gn.get_import("assay")
+    print(chunks.keys(), flush = True)
+    begin = time.time()
+    gn.adjust_transform(chunks)
+    end = time.time()
+    print("Adjusting from 1000 to 3456 took %.3f" %(end-begin), flush=True)
+    chunks = gn.new_file
+    
     print(len(chunks.keys()), flush = True)
-
-    #assay = gn.get_import("assay")
-    assay = decode_json(chunks["chunk1"])
-    data = np.array(assay.get("matrix")).T
-    print(data.shape, flush = True)
-
+    print(list(chunks.keys()), flush=True)
+    
     seed = gn.get_arg("seed")
     checkbox = gn.get_arg("use_auto_limit")
     cell_subset = gn.get_arg("cell_subset")
-
     NN_lim = {False: gn.get_arg("NN_lim"), True: "auto"}.get(checkbox, True)
+    model = MultiNet(seed=seed)
+    output = dict()
 
-    #model = MultiNet(n_cores="all", seed=seed)
-    #model.fit(data, NN_lim=NN_lim, cell_subset=cell_subset)
-
-    frameddata = pd.DataFrame(data)
-    print("imputed", flush = True)
-    imputed, model = deepImpute(frameddata, NN_lim=NN_lim, cell_subset=cell_subset)
-
+    for i in range(len(chunks)):
+        combined = gn.combine_new_chunk(chunks["chunk"+str(i+1)])
+        data = np.array(combined.get("matrix")).T
+        frameddata = pd.DataFrame(data)
+        model.fit(frameddata, NN_lim=NN_lim, cell_subset=cell_subset)
+        del combined, data, framedata
+        gc.collect()
+        if i == 3:
+            break
+    
+    fig, ax = plt.subplots(2, len(chunks))
     LABELS_PARAMS = {"fontsize": 14, "fontweight": "bold", "fontname": "serif"}
-    print("Calculate vmax", flush = True)
-    
-    #tmp data, imputed
-    data = data + 1
-    imputed = imputed + 1
+    for i in range(len(chunks)):
+        combined = gn.combine_new_chunk(chunks["chunk"+str(i+1)])
+        data = np.array(combined.get("matrix")).T
+        frameddata = pd.DataFrame(data)
+        imputed = model.predict(frameddata, imputed_only=False, policy="restore")
+        
+        vmax = np.percentile(np.log10(1 + data.flatten()), 99)
+        print("Generating Heatmap", flaush=True)
+        ax[0,i].imshow(np.log10(1 + data), aspect="auto", vmax=vmax)
+        ax[0,i].axes.yaxis.set_visible(False)
+        ax[0,i].axes.xaxis.set_visible(False)
+        plt.tight_layout(pad=-0.25)
+        ax[1,i].imshow(np.log10(1 + imputed), aspect="auto", vmax=vmax)
+        ax[1,i].axes.yaxis.set_visible(False)
+        ax[1,i].axes.xaxis.set_visible(False)
+        plt.tight_layout(pad=-0.25)
 
-    vmax = np.percentile(np.log10(data.flatten()), 99)
+        #ax[0, i].set_xlabel("Genes", **LABELS_PARAMS)
+        #ax[1, i].set_xlabel("Genes", **LABELS_PARAMS)
+        #ax[0, i].set_ylabel("Cells", **LABELS_PARAMS)
+        #ax[0, i].set_title("raw (log)", **LABELS_PARAMS)
+        #ax[1, i].set_title("imputed (log)", **LABELS_PARAMS)
+        
+        assay["matrix"] = imputed.T.to_numpy().tolist()
+        assay = compress_assay(assay)
+        output["chunk" + str(i)] = assay
+        if i == 3:
+            gn.add_current_figure_to_results("Heatmaps")
+            break
+        del combined,data,frameddata,imputed
+        gc.collect()
+            #r, p = model.score(frameddata)
+            #rows, cols = frameddata.shape
+            #nb_genes = len(set(model.targets.flatten()))
+            #def calc_dropout(matrix):
+            #    return np.sum((np.array(matrix) == 0)) * 1. / data.size
+            
+            # message = "\n".join(
+           #[
+           # "  - Data frame number of rows: **{0}**",
+           # "  - Data frame number of columns: **{1}**",
+           # "  - Number of imputed genes: **{2}**",
+           # "  - Percentage of dropout entries *before* imputation: **{3:.2f}%**",
+           # "  - Percentage of dropout entries *after* imputation: **{4:.2f}%**",
+           # "  - Accuracy (correlation) on masked data: **{5:.2f}**"
+           # ]
+           # ).format(
+           #     rows,
+           #     cols,
+           #     nb_genes,
+           #     calc_dropout(data) * 100,
+           #     calc_dropout(imputed.to_numpy()) * 100,
+           #     r
+           # )
 
-    print("Generating Heatmap", flush = True)
-    fig, ax = plt.subplots(1, 2)
-    ax[0].imshow(np.log10(data), aspect="auto", vmax=vmax)
-    ax[1].imshow(np.log10(imputed), aspect="auto", vmax=vmax)
-    ax[0].set_xlabel("Genes", **LABELS_PARAMS)
-    ax[1].set_xlabel("Genes", **LABELS_PARAMS)
-    ax[0].set_ylabel("Cells", **LABELS_PARAMS)
-    ax[0].set_title("raw (log)", **LABELS_PARAMS)
-    ax[1].set_title("imputed (log)", **LABELS_PARAMS)
-    
-    print("Heatmap created!", flush = True)
-
-    gn.add_current_figure_to_results("Heatmaps")
-    nb_genes = len(set(model.targets.flatten()))
-    #nb_genes = np.sum([len(net.targetGenes) for net in model.targets])
-    print("finished adding", flush = True)
-    #del data
-    #gc.collect()
-    def calc_dropout(matrix):
-        return np.sum((np.array(matrix) == 0)) * 1. / data.size
-
-    r, p = model.score(frameddata)
-    rows, cols = frameddata.shape
-    print("start to transform", flush = True)
-    # transform data, imputed back
-    data = data - 1
-    imputed = imputed - 1
-    print("creating message", flush = True)
-
-    message = "\n".join(
-        [
-            "  - Data frame number of rows: **{0}**",
-            "  - Data frame number of columns: **{1}**",
-            "  - Number of imputed genes: **{2}**",
-            "  - Percentage of dropout entries *before* imputation: **{3:.2f}%**",
-            "  - Percentage of dropout entries *after* imputation: **{4:.2f}%**",
-            "  - Accuracy (correlation) on masked data: **{5:.2f}**"
-        ]
-    ).format(
-        rows,
-        cols,
-        nb_genes,
-        calc_dropout(data) * 100,
-        calc_dropout(imputed.to_numpy()) * 100,
-       r
-    )
-
-    gn.add_result(message, data_type="markdown")
-    
-    del data
-    gc.collect()
-    
-    assay["matrix"] = imputed.T.to_numpy().tolist()
-    deepimpute_time = time.time()
-    print("--- %s seconds --- for deepimpute" % (deepimpute_time - start_time), flush = True)
-    print("start to compress", flush = True) 
-    assay = compress_assay(assay)
-    output["chunk1"] = assay
+           # gn.add_result(message, data_type="markdown")
     gn.export_statically(output, "Imputed assay")
-    #with open(os.path.join(gn.exports_dir,"chunks"),"wt") as f:
-    #    json.dump(output, f)
-    #gn.dynamic_exports.append({"extractFrom": "chunks", "kind": "assay", "meta": None})
     gn.commit()
-    del imputed, assay
-    gc.collect()
-    print("--- %s seconds --- for compress" % (time.time() - deepimpute_time), flush = True)
-    print("--- %s seconds --- for whole gbox" % (time.time() - start_time), flush = True)
-    time.sleep(10)
+
+    #print("--- %s seconds --- for compress" % (time.time() - deepimpute_time), flush = True)
+    #print("--- %s seconds --- for whole gbox" % (time.time() - start_time), flush = True)
+    #time.sleep(10)
 
 if __name__ == "__main__":
     main()
